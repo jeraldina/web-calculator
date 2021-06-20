9aaa2e0fa0e64ae86360b065df380c8891045435
jeraldina <jeraldina@gmail.com>
2021-06-20 15:47:36 -0400

    adding html, css and script files

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculation() {
+  let operator = document.getElementById('operator')
+  let mathOperators = operator.options[operator.selectedIndex].value
+  let answer = document.getElementById('answer')
+  let num1 = Number(document.getElementById('num1').value)
+  let num2 = Number(document.getElementById('num2').value)
+
+  switch (mathOperators) {
+    case 'add':
+      answer.innerHTML = num1 + num2;
+      break
+    
+      case 'sub':
+      answer.innerHTML = num1 - num2;
+      break
+   
+      case 'divide':
+      answer.innerHTML = num1 / num2;
+      break
+    
+      case 'multiply':
+      answer.innerHTML = num1 * num2;
+      break
+  }
+}
+
